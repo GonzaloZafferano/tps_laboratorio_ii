873fd8f7bcebcc3d1bf36300c8fe213bef9ba039
GonzaloZafferano <gonzalozafferano92@gmail.com>
2022-04-25 09:48:50 -0300

    Trabajo Practico 02

---
+++ TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        /// <summary>
+        /// Constructor de la clase Ciclomotor
+        /// </summary>
+        /// <param name="marca">Marca del vehiculo</param>
+        /// <param name="chasis">Chasis del vehiculo</param>
+        /// <param name="color">Color del vehiculo</param>     
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis, marca, color)
+        {
+        }
+        
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return Vehiculo.ETamanio.Chico;
+            }
+        }
+
+        /// <summary>
+        /// Muestra los datos del vehiculo
+        /// </summary>
+        /// <returns>Un string con los datos del vehiculo</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP2/Entidades/Vehiculo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        };
+
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        };
+
+        /// <summary>
+        /// Constructor de la clase Vehiculo
+        /// </summary>
+        /// <param name="chasis">Chasis del vehiculo</param>
+        /// <param name="marca">Marca del vehiculo</param>
+        /// <param name="color">Color del vehiculo</param>
+        public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+
+        private EMarca marca;
+        private string chasis;
+        private ConsoleColor color;
+
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio { get; }
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns>Los datos del vehiculo.</returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        /// <summary>
+        /// Castea el vehiculo a un string, que contiene los datos del mismo.
+        /// </summary>
+        /// <param name="p"></param>
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CHASIS: {0}\r\n", p.chasis);
+            sb.AppendFormat("MARCA : {0}\r\n", p.marca.ToString());
+            sb.AppendFormat("COLOR : {0}\r\n", p.color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1">Vehiculo uno</param>
+        /// <param name="v2">Vehiculo dos</param>
+        /// <returns>True si son iguales, caso contrario false.</returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1">vehiculo uno</param>
+        /// <param name="v2">vehiculo dos</param>
+        /// <returns>True si son distintos. Caso contrario false.</returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1.chasis == v2.chasis);
+        }
+    }
+}
